0ece703157fb7cb26a2785c741b5ec1ed36ad080
u-master <yurgan@gmail.com>
2020-08-05 18:09:11 +0600

    add sending message

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,24 +1,52 @@
 import React, { useState, useContext } from 'react';
+import { connect } from 'react-redux';
+import cn from 'classnames';
+import axios from 'axios';
+
+import routes from '../routes';
 
 import UserContext from '../features/user/userContext';
 
-export default () => {
+const mapStateToProps = ({ currentChannelId }) => ({ currentChannelId });
+
+const MessageForm = ({ currentChannelId }) => {
   const [text, setText] = useState('');
+  const [process, setProcess] = useState('idle');
+  const [feedback, setFeedback] = useState('');
   const { username } = useContext(UserContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`${username}: ${text}`);
-    setText('');
+    const url = routes.channelMessagesPath(currentChannelId);
+    setProcess('pending');
+    axios
+      .post(url, { data: { attributes: { text, username } } }, { timeout: 5000 })
+      .then(() => {
+        setText('');
+        setFeedback('');
+        setProcess('fulfilled');
+      })
+      .catch((error) => {
+        setProcess('rejected');
+        setFeedback(`${error.name}: ${error.message}`);
+      });
   };
 
   const handleChange = ({ target: { value } }) => {
     setText(value);
   };
 
+  const inputClasses = cn({
+    'w-100': true,
+    'is-invalid': process === 'rejected',
+  });
+
   return (
     <form onSubmit={handleSubmit}>
-      <input className="w-100" name="message" type="text" onChange={handleChange} value={text} />
+      <input className={inputClasses} name="message" type="text" onChange={handleChange} value={text} disabled={process === 'pending'} />
+      <div className="d-block invalid-feedback">{feedback}</div>
     </form>
   );
 };
+
+export default connect(mapStateToProps)(MessageForm);
