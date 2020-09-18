31e235bdc473866484e4882f0af70ad2adba6416
u-master <to-umaster@mail.ru>
2020-09-18 15:16:04 +0600

    remove olde code

--- src/index.js
+++ src/index.js
@@ -6,19 +6,13 @@ import 'regenerator-runtime/runtime';
 import '../assets/application.scss';
 import '../assets/favicon.ico';
 
-// import faker from 'faker';
 // @ts-ignore
 import gon from 'gon';
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
 
 import run from './app';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
-
-// console.log('it works!');
-// console.log('gon', gon);
 
 run(gon);
