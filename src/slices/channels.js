b9ca75a0a5efb08e5373261c409f7e583abcaf7c
u-master <to-umaster@mail.ru>
2020-10-01 00:05:54 +0600

    unite states channels and currentChannelId

--- src/slices/channels.js
+++ src/slices/channels.js
@@ -1,23 +1,33 @@
+/* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
 const channelsSlice = createSlice({
   name: 'channels',
-  initialState: [],
+  initialState: { channelsItems: [], currentChannelId: 0, defaultChannelId: 0 },
   reducers: {
-    addChannel(state, { payload: newChannel }) {
-      state.push(newChannel);
+    addChannel({ channelsItems }, { payload: newChannel }) {
+      channelsItems.push(newChannel);
     },
     removeChannel(state, { payload: { id: idRemove } }) {
-      return state.filter(({ id }) => id !== idRemove);
+      const { channelsItems, currentChannelId, defaultChannelId } = state;
+      state.channelsItems = channelsItems.filter(({ id }) => id !== idRemove);
+      if (currentChannelId === idRemove) state.currentChannelId = defaultChannelId;
     },
-    renameChannel(state, { payload: renamedChannel }) {
-      const channelIndex = state.findIndex(({ id }) => id === renamedChannel.id);
-      // eslint-disable-next-line no-param-reassign
-      state[channelIndex] = renamedChannel;
+    renameChannel({ channelsItems }, { payload: renamedChannel }) {
+      const channelIndex = channelsItems.findIndex(({ id }) => id === renamedChannel.id);
+      channelsItems[channelIndex] = renamedChannel;
+    },
+    setCurrentChannelId(state, { payload: { id } }) {
+      state.currentChannelId = id;
     },
   },
 });
 
-export const { addChannel, removeChannel, renameChannel } = channelsSlice.actions;
+export const {
+  addChannel,
+  removeChannel,
+  renameChannel,
+  setCurrentChannelId,
+} = channelsSlice.actions;
 
 export default channelsSlice.reducer;
